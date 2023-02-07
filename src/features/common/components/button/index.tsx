587e24c0602afa4a5277223b95682678de01ce80
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-07 14:28:42 -0400

    feat: add button component

---
+++ src/features/common/components/button/index.tsx
@@ -0,0 +1,25 @@
+import { ButtonHTMLAttributes } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { cva, VariantProps } from 'class-variance-authority';
+
+type TVariants = VariantProps<typeof styles>;
+
+interface IButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variants?: TVariants;
+}
+
+export function Button({ children, variants, className, ...rest }: IButton) {
+  return (
+    <button {...rest} className={twMerge(styles({ className, ...variants }))}>
+      {children}
+    </button>
+  );
+}
+
+const styles = cva('flex items-center justify-center', {
+  variants: {
+    type: {
+      icon: 'p-3 w-fit rounded-full aspect-square bg-black text-white',
+    },
+  },
+});
