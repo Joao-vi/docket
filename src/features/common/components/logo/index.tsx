09ddf1c840414c2c900632c2d7b686d3eb2cbfdf
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-08 08:15:13 -0400

    feat: add Logo component

---
+++ src/features/common/components/logo/index.tsx
@@ -0,0 +1,3 @@
+export function Logo() {
+  return <h1 className="font-semibold text-2xl tracking-tighter">Docket</h1>;
+}
--- src/features/notes/components/side-bar/index.tsx
+++ src/features/notes/components/side-bar/index.tsx
@@ -1,9 +1,11 @@
+import { Logo } from '../../../common/components/logo';
+
 import { AddNote } from '../add-note';
 
 export function SideBar() {
   return (
     <section className="p-8 flex flex-col items-center border-r-2 border-gray-100">
-      <h1 className="font-semibold text-2xl">Notes</h1>
+      <Logo />
 
       <AddNote />
     </section>
