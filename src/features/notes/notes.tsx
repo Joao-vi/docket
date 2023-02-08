6fc3eb7d45f777f8e39c9e426884c3d47fa625d4
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-08 11:43:05 -0400

    chore: delete border on Notes component

--- src/features/notes/notes.tsx
+++ src/features/notes/notes.tsx
@@ -3,7 +3,7 @@ import { SideBar } from './components/side-bar';
 
 export function Notes() {
   return (
-    <main className="h-full flex gap-10 border border-black">
+    <main className="h-full flex gap-10">
       <SideBar />
 
       <MainContent />
